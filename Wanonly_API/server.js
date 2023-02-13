6a81a56982e13949a317062441d36e4d4c47b3d9
Nuttapong Boonsala <nuttapongboonsala@gmail.com>
2023-02-13 16:26:25 +0700

    change API port to 3000

--- Wanonly_API/server.js
+++ Wanonly_API/server.js
@@ -10,7 +10,7 @@ mongoose.connect(
     { useNewUrlParser: true}
 )
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 // const port = process.env.PORT;
 const app = express();
 
